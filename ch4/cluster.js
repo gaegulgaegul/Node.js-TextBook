b8ea1c88eb1c6bc38f691800ce74456f1b03c1c1
linmingseay <dla9721@gmail.com>
2018-11-04 10:59:27 +0900

    클러스터 수정

--- ch4/cluster.js
+++ ch4/cluster.js
@@ -1,11 +1,11 @@
 const cluster = require('cluster');
 const http = require('http');
-const numCPUS = require('os').cpus().length;
+const numCPUs = require('os').cpus().length;
 
 if(cluster.isMaster) {
     console.log('마스터 프로세스 아이디: ${process.pid}');
     // CPU 개수만큼 워커를 생산
-    for(let i=0; i<numCPUS; i+=1) {
+    for(let i=0; i<numCPUs; i+=1) {
         cluster.fork();
     }
     // 워커가 종료되었을 때
